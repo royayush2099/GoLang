282fa756c385c002f8e9167acc4993b1d6654b49
Ayushroy <royayush2099@gmail.com>
2024-10-22 20:42:27 +0530

    vars

---
+++ OneDrive/Desktop/Go/02variables/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "Ayush Roy "
+	var userId int = 15009
+	fmt.Println(username, "Id:", userId)
+	fmt.Printf("variable username is of type: %T \n", username)
+	var isLoggedIn bool = false
+	fmt.printf("variable is of type: %T \n", isLoggedIn)
+}
