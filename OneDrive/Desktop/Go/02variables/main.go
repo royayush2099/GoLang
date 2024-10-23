fed09f84a623642fc7a1fff6dab9f4905acba072
Ayushroy <royayush2099@gmail.com>
2024-10-23 20:56:02 +0530

    streak

--- OneDrive/Desktop/Go/02variables/main.go
+++ OneDrive/Desktop/Go/02variables/main.go
@@ -7,6 +7,7 @@ func main() {
 	var userId int = 15009
 	fmt.Println(username, "Id:", userId)
 	fmt.Printf("variable username is of type: %T \n", username)
-	var isLoggedIn bool = false
-	fmt.printf("variable is of type: %T \n", isLoggedIn)
+	var smallval bool = false
+	fmt.Printf("variable is of type: %T \n", smallval)
+
 }
