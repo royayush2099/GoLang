f51fc882e39c514dc5521a096d71e1e27bab057d
Ayushroy <royayush2099@gmail.com>
2024-10-25 20:11:36 +0530

    learnt about packages

---
+++ OneDrive/Desktop/Go/03userinput/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	welcome := "welcome to user input"
+	fmt.Println(welcome)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for our pizza:")
+
+	//comma ok syntax // err ok
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating,", input)
+	fmt.Printf("type of this rating is %T", input)
+}
